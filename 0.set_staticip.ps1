116aba39f4bb5eda878937cf570c08cd45e234d9
Sergey Korneev <s_korneev@wargaming.net>
2019-09-19 13:17:24 +0300

    test

---
+++ 0.set_staticip.ps1
@@ -0,0 +1,20 @@
+$interface_data = Get-NetIPAddress | where IPAddress -like "172*"
+
+if ( $interface_data -is [System.Array]) {
+    Write-Host "Found more than one interface"
+    Exit
+}
+
+$default_gateway = Get-NetRoute -DestinationPrefix 0.0.0.0/0 | Select-Object -ExpandProperty NextHop
+$dns_servers =  Get-DnsClientServerAddress | where ServerAddresses -like "172.*" | Select-Object -ExpandProperty ServerAddresses
+
+Write-Host "Set $($interface_data.IPAddress)/$($interface_data.PrefixLength) gateway $default_gateway to interface $($interface_data.InterfaceIndex)"
+Remove-NetIPAddress -InterfaceIndex $interface_data.InterfaceIndex -Confirm:$false
+New-NetIPAddress -InterfaceIndex $interface_data.InterfaceIndex -IPAddress $interface_data.IPAddress -PrefixLength $interface_data.PrefixLength -DefaultGateway $default_gateway
+
+Write-Host "Set DNS: $dns_servers"
+Set-DnsClientServerAddress -InterfaceIndex $interface_data.InterfaceIndex -ServerAddresses $dns_servers
+
+Rename-Computer -NewName "dc"
+Restart-Computer
+

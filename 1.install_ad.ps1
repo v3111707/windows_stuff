a078f74ffa21ff36df701b85aaae4e5e6746b372
Sergey Korneev <s_korneev@wargaming.net>
2019-09-19 13:16:51 +0300

    first

---
+++ 1.install_ad.ps1
@@ -0,0 +1,15 @@
+$domain = "contoso.loc"
+$netbios = "CONTOSO"
+
+Install-WindowsFeature –Name AD-Domain-Services -IncludeManagementTools
+
+Install-ADDSForest  -DomainName $domain -CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "7" `
+-DomainNetbiosName $netbios `
+-ForestMode "7" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$True `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
